7c85aaaf9dbabe2c7327a3f2660d132699e8a58d
Magdalena Kuncicka <mkuncicka@gmail.com>
2018-02-11 20:08:05 +0100

    CreateRecipe unit tests, empty classes for other tests

---
+++ tests/unit/Cookway/Infrastructure/Dictionary/Query/UnitDictionaryQueryTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created By Magdalena Kuncicka
+ * Copyright (c) 2018
+ *
+ * All Rights Reserved
+ */
+
+namespace tests\unit\Cookway\Infrastructure\Dictionary\Query;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * UnitDictionaryQuery unit tests
+ *
+ * @author Magdalena Kuncicka <mkuncicka@gmail.com>
+ */
+class UnitDictionaryQueryTest extends TestCase
+{
+
+}
---
+++ tests/unit/Cookway/Infrastructure/Recipe/Query/UsersRecipeListQueryTest.php
@@ -0,0 +1,21 @@
+<?php
+/**
+ * Created By Magdalena Kuncicka
+ * Copyright (c) 2018
+ *
+ * All Rights Reserved
+ */
+
+namespace tests\unit\Cookway\Infrastructure\Recipe\Query;
+
+use PHPUnit\Framework\TestCase;
+
+/**
+ * UsersRecipeListQuery unit tests
+ *
+ * @author Magdalena Kuncicka <mkuncicka@gmail.com>
+ */
+class UsersRecipeListQueryTest extends TestCase
+{
+
+}
